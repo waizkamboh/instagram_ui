7f050a69af60f13bfe88c272f1d797fc27ee38b8
waizkamboh <waizkamboh6@gmail.com>
2025-08-15 15:43:56 +0500

    Instagram Top bar and stories text

---
+++ lib/home_screen.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 15),
+          child: Column(
+            children: [
+              instagram(),
+              storiesText()
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget instagram() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Icon(Icons.photo_camera_outlined,size: 30,color: Colors.black54,),
+        Image.asset("assets/images/instagram.png",width: 150,),
+        Image.asset("assets/images/send1.png",width: 30,),
+      ],
+    );
+  }
+
+  Widget storiesText() {
+    return Row(
+      children: [
+        Text("Stories",style: TextStyle(fontWeight: FontWeight.bold),),
+        Spacer(),
+        Icon(Icons.play_arrow,size: 20,),
+        SizedBox(width: 5,),
+        Text("Watch all",style: TextStyle(fontWeight: FontWeight.bold),),
+
+      ],
+    );
+  }
+}
